5f8b020a6f353c7ce2388d824eab3d9f495b0b57
cwandev <18888351756@163.com>
2025-10-31 15:44:27 +0800

    chore: update deps

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router/auto'
+import { createRouter, createWebHistory } from 'vue-router'
 import { handleHotUpdate, routes } from 'vue-router/auto-routes'
 
 import NProgress from 'nprogress'
